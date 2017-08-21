638c78715248b174bc50689c33224c11f64d53a4
Javed <jdecosta@gmail.com>
2017-08-21 13:05:02 +1000

    html tidy

--- models/pair.rb
+++ models/pair.rb
@@ -1,32 +1,25 @@
 class Pair < ActiveRecord::Base
-
   def phrase
     words = []
 
-    words.push Adjective.find(self.adjective_1_id).word
+    words.push Adjective.find(adjective_1_id).word
 
-    if self.adjective_2_id != nil
-      words.push Adjective.find(self.adjective_2_id).word
-    end
+    words.push Adjective.find(adjective_2_id).word unless adjective_2_id.nil?
 
-    if self.noun_1_id != nil
-      words.push Noun.find(self.noun_1_id).word
-    end
+    words.push Noun.find(noun_1_id).word unless noun_1_id.nil?
 
     # n2 = Noun.find(self.noun_2_id).word
-
 
     words.join(' ')
   end
 
   def size
-    if self.adjective_2_id == nil && self.noun_2_id == nil
-      return 2
-    elsif self.adjective_2_id != nil && self.noun_2_id == nil
-      return 3
+    if adjective_2_id.nil? && noun_2_id.nil?
+      2
+    elsif !adjective_2_id.nil? && noun_2_id.nil?
+      3
     else
-      return 4
+      4
     end
   end
-
 end
